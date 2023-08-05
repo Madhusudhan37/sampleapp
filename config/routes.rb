f486fad5cd94237ce7fac19f8ef29117334eb67d
Madhusudhan37 <madhusudhanarao360@gamil.com>
2023-08-05 11:53:46 +0530

    cw-085 employee info update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise for :users
+  
+  get 'employee/info'
   get 'homes/index'
-  root " homes#index"
-    
+  root "homes#index"
+  devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_04_043956) do
+ActiveRecord::Schema.define(version: 2023_08_05_060023) do
+
+  create_table "employees", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "salary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
